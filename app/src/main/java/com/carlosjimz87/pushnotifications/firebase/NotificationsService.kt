bfca4f1136a10a1649748c9ee3fa1ef0c3a0c16b
Carlos Jiménez <carlos.jimenezp@vass.es>
2023-09-09 04:09:19 +0200

    prepare notifications service

--- app/src/main/java/com/carlosjimz87/pushnotifications/firebase/NotificationsService.kt
+++ app/src/main/java/com/carlosjimz87/pushnotifications/firebase/NotificationsService.kt
@@ -2,10 +2,17 @@ package com.carlosjimz87.pushnotifications.firebase
 
 import android.util.Log
 import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
 
 class NotificationsService : FirebaseMessagingService(){
     // generate a notification
     // attach the notification created with the custom layout
     // show the notification
+    override fun onMessageReceived(message: RemoteMessage) {
+        super.onMessageReceived(message)
+    }
 
+    override fun onNewToken(token: String) {
+        super.onNewToken(token)
+    }
 }

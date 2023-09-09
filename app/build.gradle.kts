a456bd09eae7649e8179a42b5579beb420902cac
Carlos Jiménez <carlos.jimenezp@vass.es>
2023-09-09 04:08:32 +0200

    add firebase messaging lib, refactor to firebase folder

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -71,5 +71,6 @@ dependencies {
 
     // Firebase
     implementation(platform("com.google.firebase:firebase-bom:32.2.3"))
-    implementation("com.google.firebase:firebase-analytics-ktx")
+    implementation("com.google.firebase:firebase-analytics-ktx:21.3.0")
+    implementation("com.google.firebase:firebase-messaging-ktx:23.2.1")
 }
--- app/src/main/java/com/carlosjimz87/pushnotifications/firebase/NotificationsService.kt
+++ app/src/main/java/com/carlosjimz87/pushnotifications/firebase/NotificationsService.kt
@@ -1,4 +1,4 @@
-package com.carlosjimz87.pushnotifications
+package com.carlosjimz87.pushnotifications.firebase
 
 import android.util.Log
 import com.google.firebase.messaging.FirebaseMessagingService

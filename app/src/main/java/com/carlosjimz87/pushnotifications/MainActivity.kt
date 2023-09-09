80e324d8c0a43fcebea12eea97c1b1c6241102c2
Carlos Jiménez <carlos.jimenezp@vass.es>
2023-09-09 03:58:24 +0200

    config firebase and change layout

--- app/src/main/java/com/carlosjimz87/pushnotifications/MainActivity.kt
+++ app/src/main/java/com/carlosjimz87/pushnotifications/MainActivity.kt
@@ -3,13 +3,20 @@ package com.carlosjimz87.pushnotifications
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import com.carlosjimz87.pushnotifications.ui.theme.PushNotificationsTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,11 +25,8 @@ class MainActivity : ComponentActivity() {
         setContent {
             PushNotificationsTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
+                Surface(color = MaterialTheme.colorScheme.background) {
+                    Greeting("Push Notifications \n 😎 !!!")
                 }
             }
         }
@@ -31,10 +35,18 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+
+    ) {
+        Text(
+            text = "Hello $name !!!", modifier = modifier,
+            style = TextStyle(fontSize = 30.sp, fontWeight = FontWeight.Bold)
+        )
+    }
+
 }
 
 @Preview(showBackground = true)
